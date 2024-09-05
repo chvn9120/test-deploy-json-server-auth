3b1274a96c77c9994cdd0485afb23f0692296f00
WiBuMoney <cowsmile1911@gmail.com>
2024-09-05 18:04:35 +0700

    *Update - Allow write operations

--- api/server.js
+++ api/server.js
@@ -4,15 +4,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('auth.db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('auth.db.json')
+// const router = jsonServer.router('auth.db.json')
 
 const middlewares = jsonServer.defaults()
 
